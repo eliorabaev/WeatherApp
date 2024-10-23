5d80d552e2e7c8b3261b3de9880f990a1b25fc12
Elior Abaev <elior.abaev@gmail.com>
2024-10-23 09:25:05 +0300

    Update to index.js file

--- api/index.js
+++ api/index.js
@@ -4,12 +4,10 @@ import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const app = express();
-const port = 3000;
 const apiKey = process.env.API_KEY;
 const apiUrl = "http://api.weatherapi.com/v1/current.json?";
-var flag = false;
+let flag = false;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +17,7 @@ app.get("/", async (req, res) => {
         const result = await axios.get(`${apiUrl}key=${apiKey}&q=auto:ip`);
         flag = true;
         res.render("index.ejs", {
-            flag:flag,
+            flag: flag,
             location: result.data.location.name,
             localtime: result.data.location.localtime,
             temp: result.data.current.temp_c,
@@ -29,7 +27,7 @@ app.get("/", async (req, res) => {
             humidity: result.data.current.humidity,
             uv: result.data.current.uv,
         });
-      } catch (error) {
+    } catch (error) {
         res.render("index.ejs", {
             flag: false,
             location: null,
@@ -53,16 +51,16 @@ app.get("/get-weather", async (req, res) => {
         const result = await axios.get(`${apiUrl}key=${apiKey}&q=${city}`);
         flag = false;
         res.render("index.ejs", {
-          flag: flag,
-          location: result.data.location.name,
-          localtime: result.data.location.localtime,
-          temp: result.data.current.temp_c,
-          condition: result.data.current.condition.text,
-          icon: result.data.current.condition.icon,
-          wind: result.data.current.wind_kph,
-          humidity: result.data.current.humidity,
-          uv: result.data.current.uv,
-          error: null
+            flag: flag,
+            location: result.data.location.name,
+            localtime: result.data.location.localtime,
+            temp: result.data.current.temp_c,
+            condition: result.data.current.condition.text,
+            icon: result.data.current.condition.icon,
+            wind: result.data.current.wind_kph,
+            humidity: result.data.current.humidity,
+            uv: result.data.current.uv,
+            error: null
         });
     } catch (error) {
         res.render("index.ejs", {
